80f2b9e54f78e4b96f0c1b202d075aa9edbfbe31
NOOB <codphantom205@gmail.com>
2025-12-19 16:06:27 +0530

    Update Square.py

--- Square.py
+++ Square.py
@@ -1,4 +1,4 @@
-a=int(input("Enter a number : "))
-b=int(input("Enter a number : "))
-even_square = {x**2 for x in range (a,b+1) if x%2 ==0}
+a= int(input("Enter a number :"))
+b= int(input("Enter a number :"))
+even_square=[x**2 for x in range (a,b+1) if x%2==0]
 print(even_square)
