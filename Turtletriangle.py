3f8e002e6944bcd9d99eeb5cc5c1b400d3f8446c
NOOB <codphantom205@gmail.com>
2025-12-19 16:02:45 +0530

    Add files via upload

---
+++ Turtletriangle.py
@@ -0,0 +1,15 @@
+import turtle as t
+t.pensize(8)
+t.pendown()
+for i in range (3):
+    t.fd(300)
+    t.rt(-120)
+t.fd(-200)
+for i in range (3):
+    t.fd(200)
+    t.rt(-120)
+t.fd(-100)
+for i in range (3):
+    t.fd(100)
+    t.rt(-120)
+t.done()
